074043b0c03f7a514833463d00d512bf9096324a
Muhammad Ijlal Nasir <ijlalfarooqi30@gmail.com>
2020-07-04 14:02:04 +0500

    add project files till part 5
    This commit contains all the project files till part 5 of the tutorial series

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Blogging Application in PHP</title>
+	<link rel="stylesheet" href="./style.css">
+</head>
+<body>
+	<nav class="navbar">
+		<div class="logo">SimpleBlog</div>
+		<ul class="nav-links">
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">Create Blog</a></li>
+		</ul>
+	</nav>
+
+	<main>
+		<section class="main-banner">
+			<h1>All Articles</h1>
+		</section>
+
+		<section class="blogs-wrapper">
+			<div class="blogs">
+				<div class="blog">
+					<div class="blog-body">
+						<h4>Blog Title</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum corrupti necessitatibus quisquam enim ipsam fuga obcaecati praesentium quia deleniti numquam.</p>
+						<a class="blog-btn" href="#">Read More</a>
+					</div>
+				</div>
+
+				<div class="blog">
+					<div class="blog-body">
+						<h4>Blog Title</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum corrupti necessitatibus quisquam enim ipsam fuga obcaecati praesentium quia deleniti numquam.</p>
+						<a class="blog-btn" href="#">Read More</a>
+					</div>
+				</div>
+
+				<div class="blog">
+					<div class="blog-body">
+						<h4>Blog Title</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum corrupti necessitatibus quisquam enim ipsam fuga obcaecati praesentium quia deleniti numquam.</p>
+						<a class="blog-btn" href="#">Read More</a>
+					</div>
+				</div>
+
+				<div class="blog">
+					<div class="blog-body">
+						<h4>Blog Title</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum corrupti necessitatibus quisquam enim ipsam fuga obcaecati praesentium quia deleniti numquam.</p>
+						<a class="blog-btn" href="#">Read More</a>
+					</div>
+				</div>
+
+				<div class="blog">
+					<div class="blog-body">
+						<h4>Blog Title</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum corrupti necessitatibus quisquam enim ipsam fuga obcaecati praesentium quia deleniti numquam.</p>
+						<a class="blog-btn" href="#">Read More</a>
+					</div>
+				</div>
+
+				<div class="blog">
+					<div class="blog-body">
+						<h4>Blog Title</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum corrupti necessitatibus quisquam enim ipsam fuga obcaecati praesentium quia deleniti numquam.</p>
+						<a class="blog-btn" href="#">Read More</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+
+	<footer class="footer">
+		<p>All rights reserved 2020 simpleBlog</p>
+	</footer>
+	
+</body>
+</html>
